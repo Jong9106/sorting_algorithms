057c316cc87ca417c152cb680d3e88a9a850f732
Jong9106 <juank_hr@hotmail.com>
2021-02-03 19:27:11 -0500

    Fucntion to sort a doubly list in ascending

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Function that sorts a doubly list in ascending order
+ * @list: list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list;
+	listint_t *temp;
+
+	while (current)
+	{
+		temp = current->prev;
+
+		while (temp)
+		{
+			if (temp->n > current->n)
+			{
+				if (temp->prev)
+					temp->prev->next = current;
+				if (current->next)
+					current->next->prev = temp;
+
+				temp->next = current->next;
+				current->next = temp;
+				current->prev = temp->prev;
+				temp->prev = current;
+				if (!current->prev)
+					*list = current;
+				print_list(*list);
+			}
+			temp = temp->prev;
+		}
+		current = current->next;
+	}
+}
